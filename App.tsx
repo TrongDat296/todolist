a7a7a15d1209cf18c1512cb84f29d9b96c505d58
TrongDat296 <huynhtrong.0611@gmail.com>
2024-02-22 15:03:17 +0700

    firebase auth

--- App.tsx
+++ App.tsx
@@ -1,27 +1,29 @@
-import { View, Text, StyleSheet, Button, StatusBar, SafeAreaView } from 'react-native'
-import React from 'react'
-import { colors } from './src/constants/colors'
-import { NavigationContainer } from '@react-navigation/native'
-import Router from './src/routers/Router'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  StatusBar,
+  SafeAreaView,
+} from 'react-native';
+import React from 'react';
+import {colors} from './src/constants/colors';
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './src/routers/Router';
 
 const App = () => {
   return (
     <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor={colors.bgColor} />
+      <SafeAreaView style={{flex: 1}}>
+        <StatusBar barStyle="light-content" backgroundColor={colors.bgColor} />
         <NavigationContainer>
           <Router />
         </NavigationContainer>
       </SafeAreaView>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({
-
-
-})
+const styles = StyleSheet.create({});
